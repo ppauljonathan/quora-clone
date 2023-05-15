49067ea00e35341515c215b01c70b3a29f464d6b
P Paul Jonathan <ppauljonathan@gmail.com>
2023-05-15 12:51:47 +0530

    made user profile amd user edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: :show
+  before_action :set_user, only: %i[show edit update]
 
   def index
     @users = User.all
   end
 
   def show
-    @topics = ('aa'..'dd').to_a
+    @topics = ['Science & Technology', 'Mathematics', 'Space']
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: 'succesfully updated'
+    else
+      flash.now[:error] = @user.errors
+      render :edit, status: 422
+    end
   end
 
   private def set_user
     @user = User.find(params[:id])
+  end
+
+  private def user_params
+    params.require(:user).permit(:name, :profile_picture)
   end
 end
