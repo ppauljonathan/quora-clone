595c4a63148c2bc639e68aab65d438583cc92560
P Paul Jonathan <70207656+ppauljonathan@users.noreply.github.com>
2023-06-07 19:07:37 +0530

    Update rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 # need to enable dev:cache for this to work
-LIMIT = 3
-PERIOD = 10.seconds
+LIMIT = 100
+PERIOD = 1.hour
 PUBLIC_API_REGEX = %r{^/api/topics/+}
 
 Rack::Attack.throttle('reqests to api', limit: LIMIT, period: PERIOD) do |req|
