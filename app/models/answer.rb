d2b1276186fcb337a92b2807e00a1b44054ada60
P Paul Jonathan <ppauljonathan@gmail.com>
2023-06-12 17:50:01 +0530

    added publish method to AbuseReportable callback

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,8 +6,6 @@ class Answer < ApplicationRecord
   has_many :comments, as: :commentable
   has_rich_text :content
 
-  before_save { self.published_at = Time.now }
-
   validates :content, presence: true
 
   default_scope { order created_at: :desc }
