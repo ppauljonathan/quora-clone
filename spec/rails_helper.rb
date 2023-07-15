2684c78be1bb0ec9a12b863c27eb9d486d311b57
P Paul Jonathan <ppauljonathan@gmail.com>
2023-07-15 16:53:25 +0530

    minor change in rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,11 +2,11 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
